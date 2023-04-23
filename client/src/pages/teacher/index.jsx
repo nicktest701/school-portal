b3de2a8e095ed6dc36a4d976858920f0d3053d9d
nicktest701 <nicktest701@gmail.com>
2023-04-23 23:58:26 +0000

    demo3

--- client/src/pages/teacher/index.jsx
+++ client/src/pages/teacher/index.jsx
@@ -1,18 +1,11 @@
-import React, { useContext } from "react";
-import { Outlet } from "react-router-dom";
-import TeacherAlert from "../../components/alerts/TeacherAlert";
-import { TeacherContext } from "../../context/providers/TeacherProvider";
-import TeacherEdit from "./TeacherEdit";
-import TeacherView from "./TeacherView";
+
+import { Outlet } from 'react-router-dom';
+import TeacherEdit from './TeacherEdit';
+import TeacherView from './TeacherView';
 
 const Teacher = () => {
-  const {
-    teacherState: { alertData },
-  } = useContext(TeacherContext);
-
   return (
     <>
-      {alertData?.message && <TeacherAlert />}
       <Outlet />
       <TeacherView />
       <TeacherEdit />
